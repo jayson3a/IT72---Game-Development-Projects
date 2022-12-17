13eb618aecaa6d66f1c14624050e59cb3f3e9a95
Jayson Rey Humilde <jaysonreyhumilde@gmail.com>
2022-12-17 18:45:34 +0800

    PRELIM
    School Activities

---
+++ 3D CUBE/Assets/spinning_cube.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinning_cube : MonoBehaviour
+{
+    public Vector3 RotateAmount;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateAmount);
+    }
+}
